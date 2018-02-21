48181293b8ef4ac8aa12d41ad896028ad3ff4ed6
Brannon Dorsey <brannon@brannondorsey.com>
2018-02-21 16:37:07 -0600

    Fix start_bitcoind.sh bug where the wrong bitcoin.conf was being used.

--- start_bitcoind.sh
+++ start_bitcoind.sh
@@ -1,4 +1,9 @@
 # /bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# for some some of these zmq messages don't run if configured in the .conf
+# so instead we will enable them manually as a cli args
+
 bitcoind \
---conf=./bitcoin.conf \
---datadir=/media/bbpwn2/BBPWN_BACKUP/.bitcoin
+-conf="${DIR}/bitcoin.conf" \
+-datadir=/media/bbpwn2/BBPWN_BACKUP/.bitcoin
