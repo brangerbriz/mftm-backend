4f262206340af3a7bedf03d28b67f784eaab41a9
Brannon Dorsey <brannon@brannondorsey.com>
2018-05-10 10:14:09 -0500

    Add forever as a dev dependency and update launch-mftm-web.sh to use it.

--- launch-mftm-web.sh
+++ launch-mftm-web.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SCRIPT="/home/ubuntu/.nvm/versions/node/v8.11.1/bin/node /home/ubuntu/messages-from-the-mines/mftm-backend/server.js"
-LOGFILE=/home/ubuntu/messages-from-the-mines/mftm-backend/log/mftm.log
-PIDFILE=/home/ubuntu/messages-from-the-mines/mftm-backend/log/mftm.pid
+SCRIPT="sudo forever -c node server.js"
+LOGFILE=./log/mftm.log
+PIDFILE=./log/mftm.pid
 
 # the server was written to be run from the project root
 cd /home/ubuntu/messages-from-the-mines/mftm-backend
